47570a52e20f369c453aef35bab4b6f161a3a722
Juan Carlos Pérez <perezpardojc@gmail.com>
2015-11-13 18:40:14 +0000

    aa

--- Parse_Parameters/parse.py
+++ Parse_Parameters/parse.py
@@ -1,4 +1,11 @@
+#
+#
+#
+
+
 import argparse
+
+
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('integers', metavar='N', type=int, nargs='+',
@@ -8,4 +15,7 @@ parser.add_argument('--sum', dest='accumulate', action='store_const',
                    help='sum the integers (default: find the max)')
 
 args = parser.parse_args()
+
+
+
 print args.accumulate(args.integers)
